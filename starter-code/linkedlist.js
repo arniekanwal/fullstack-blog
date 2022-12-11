b35fdc4534ac1b6a7a200bbb079bba3d77c2e95b
arniekanwal <arnavkanwal@gmail.com>
2022-12-10 23:02:15 -0500

    prototype linked list

---
+++ starter-code/linkedlist.js
@@ -0,0 +1,42 @@
+class Node {
+    constructor(data, next = null, prev = null) {
+        this.data = data;
+        this.next = next;
+        this.prev = prev;
+    }
+}
+
+// Create a Doubly Linked List
+class LinkedList {
+    constructor(initializer_list = null) {
+        if (initializer_list) {
+            // TODO;
+        } else {
+            this.head = null;
+            this.tail = null;
+        }
+    }
+
+
+    // ==================
+    // Methods for LL
+    // ==================
+    
+    // Insert at head
+
+    // Insert at tail
+
+    // Insert at index
+
+    // Get at index
+
+    // Delete at head
+
+    // Delete at tail
+
+    // Delete at index
+
+    // Clear list
+
+    // Print list
+}
