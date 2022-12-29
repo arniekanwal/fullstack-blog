3aa9772243f506b5fff636ad55c722644def1335
arniekanwal <arnavkanwal@gmail.com>
2022-12-28 18:11:50 -0700

    change structure for blog, revert readme

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className='titlebar'> 
+            <div className='localdate'>
+                {/* TODO */}
+            </div>
+
+            <div className='title'>
+                <h1>The Anonymous American</h1>
+            </div>
+
+            <div className='login'>
+                {/* TODO */}
+            </div>
+        </div>
+    );
+}
+
+export default App;
